963d13232c88b51b801534182e4fdd6bbc389196
Albatriti <albatritalushaj10@gmail.com>
2025-01-22 22:22:44 +0100

    ndryshime disa detajeve te vogla

--- models.js
+++ models.js
@@ -2,27 +2,27 @@ const carInfo = {
     ferrari: {
         name: 'Ferrari 488 GTB',
         price: '$262,647',
-        performance: '3.9L V8 engine, 0-100 kmh in 3.0 seconds'
+        performance: '3.9L V8 engine, 0-100 kmh per 3.0 sekonda'
     },
     lamborghini: {
         name: 'Lamborghini Aventador',
         price: '$393,695',
-        performance: '6.5L V12 engine, 0-100 kmh in 2.9 seconds'
+        performance: '6.5L V12 engine, 0-100 kmh per 2.9 sekonda'
     },
     porsche: {
         name: 'Porsche 911 Turbo',
         price: '$170,800',
-        performance: '3.8L 6-cylinder engine, 0-100 kmh in 2.7 seconds'
+        performance: '3.8L 6-cylinder engine, 0-100 kmh per 2.7 sekonda'
     },
     bugatti: {
         name: 'Bugatti Chiron',
         price: '$3,000,000',
-        performance: '8.0L W16 engine, 0-100 kmh in 2.4 seconds'
+        performance: '8.0L W16 engine, 0-100 kmh per 2.4 sekonda'
     },
     mclaren: {
         name: 'McLaren 720S',
         price: '$299,000',
-        performance: '4.0L V8 engine, 0-100 mph in 2.8 seconds'
+        performance: '4.0L V8 engine, 0-100 kmh per 2.8 sekonda'
     }
 };
 
